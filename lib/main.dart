8f3eb71ba8c550104af0c4180d7d33ca6578d761
Harmanjit Singh <harmanjit140500@gmail.com>
2020-07-15 06:17:23 +0530

    basic frame done

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MaleWid extends StatelessWidget {
               'MALE',
               style: TextStyle(
                   fontSize: 18,
-                  fontWeight: FontWeight.bold,
+                  //fontWeight: FontWeight.bold,
                   color: Colors.white),
             )
           ],
@@ -76,7 +76,7 @@ class FemaleWid extends StatelessWidget {
               'FEMALE',
               style: TextStyle(
                   fontSize: 18,
-                  fontWeight: FontWeight.bold,
+                  //fontWeight: FontWeight.bold,
                   color: Colors.white),
             )
           ],
@@ -91,6 +91,23 @@ class FemaleWid extends StatelessWidget {
   }
 }
 
+class MainBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.grey[800],
+        ),
+      ),
+    );
+  }
+}
+
 class BmiCal extends StatefulWidget {
   @override
   _BmiCalState createState() => _BmiCalState();
@@ -99,8 +116,30 @@ class BmiCal extends StatefulWidget {
 class _BmiCalState extends State<BmiCal> {
   Color inactive = Colors.grey[800];
   Color active = Colors.grey[900];
+  Color finWidM = Colors.grey[800];
+  Color finWidF = Colors.grey[800];
   int male = 0;
   int female = 0;
+
+  void updatecol(int x) {
+    if (x == 1) {
+      if (finWidM == inactive) {
+        finWidM = active;
+        finWidF = inactive;
+      } else {
+        finWidM = inactive;
+        finWidF = inactive;
+      }
+    } else if (x == 0) {
+      if (finWidF == inactive) {
+        finWidF = active;
+        finWidM = inactive;
+      } else {
+        finWidF = inactive;
+        finWidM = inactive;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,22 +156,60 @@ class _BmiCalState extends State<BmiCal> {
               children: <Widget>[
                 Expanded(
                   child: GestureDetector(
-                    onTap: null,
+                    onTap: () {
+                      setState(() {
+                        updatecol(1);
+                      });
+                    },
                     child: MaleWid(
-                      boxCol: inactive,
+                      boxCol: finWidM,
                     ),
                   ),
                 ),
                 Expanded(
                   child: GestureDetector(
-                    onTap: null,
+                    onTap: () {
+                      setState(() {
+                        updatecol(0);
+                      });
+                    },
                     child: FemaleWid(
-                      boxCol: active,
+                      boxCol: finWidF,
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            MainBox(),
+            Row(
+              children: <Widget>[
+                MaleWid(boxCol: inactive),
+                FemaleWid(
+                  boxCol: inactive,
+                ),
+              ],
+            ),
+            FlatButton(
+              padding: EdgeInsets.all(0),
+              onPressed: null,
+              child: Expanded(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  width: double.infinity,
+                  height: 70,
+                  color: Colors.teal[600],
+                  child: Center(
+                    child: Text(
+                      'SUBMIT',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
