59d8b423ef336650c8ceeddcb8b9b16644618c7f
Harmanjit Singh <harmanjit140500@gmail.com>
2020-07-20 03:39:40 +0530

    all done

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'BMI Calculator',
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
